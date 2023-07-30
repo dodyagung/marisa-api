ba8695d4857427ea20eb02b5e2d7f291e9b5026f
Dody <dodyags@gmail.com>
2023-07-30 12:06:44 +0700

    disable eslint for unused var

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -14,6 +14,7 @@ export class AuthService {
     const user = await this.userService.findByUsername(username);
 
     if (user && (await bcrypt.compare(password, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const { password, ...result } = user;
       return result;
     }
